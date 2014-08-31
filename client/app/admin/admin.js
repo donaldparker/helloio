a711d6acd558ec2b1b08b2aad6305e1df84f92f4
Donald Parker <DonaldParkerII@gmail.com>
2014-08-31 09:16:24 -0700

    Init Commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('helloioApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ client/app/main/main.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('helloioApp')
+  .controller('MainCtrl', function ($scope, $http, socket) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+      socket.syncUpdates('thing', $scope.awesomeThings);
+    });
+
+    $scope.addThing = function() {
+      if($scope.newThing.name === '') {
+        return;
+      }
+      $http.post('/api/things', { name: $scope.newThing.name , info: $scope.newThing.info});
+      $scope.newThing = '';
+    };
+
+    $scope.deleteThing = function(thing) {
+      $http.delete('/api/things/' + thing._id);
+    };
+
+    $scope.$on('$destroy', function () {
+      socket.unsyncUpdates('thing');
+    });
+  });
